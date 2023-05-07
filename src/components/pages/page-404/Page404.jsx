48d7c1d3576b3fbc6ef9d3595226e992e0c3b3d3
= <alexanderglazzkov@gmail.com>
2023-05-07 16:12:20 +0700

    created temporary projects page, 404 page

---
+++ src/components/pages/page-404/Page404.jsx
@@ -0,0 +1,5 @@
+import { StyledPage404 } from './Page404.style';
+
+export const Page404 = () => {
+  return <StyledPage404>Страница не найдена</StyledPage404>;
+};
---
+++ src/components/pages/projects-page/ProjectsPage.jsx
@@ -0,0 +1,5 @@
+import { StyledProjectsPage } from './ProjectsPage.style';
+
+export const ProjectsPage = () => {
+  return <StyledProjectsPage>projects page</StyledProjectsPage>;
+};
