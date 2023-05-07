f93a061f03e9c6d76da5fa3e69c16d3e2f27cd3f
= <alexanderglazzkov@gmail.com>
2023-05-07 15:56:41 +0700

    created logo icon, mobile menu icons

---
+++ src/components/icons/BurgerCloseIcon.jsx
@@ -0,0 +1,8 @@
+export const BurgerCloseIcon = () => {
+  return (
+    <svg width='35' height='35' viewBox='0 0 35 35' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M32.9465 32.9465L1.99996 1.99996' stroke='currentColor' strokeWidth='3' strokeLinecap='round' />
+      <path d='M2.00293 33L32.9495 2.05342' stroke='currentColor' strokeWidth='3' strokeLinecap='round' />
+    </svg>
+  );
+};
---
+++ src/components/icons/BurgerOpenIcon.jsx
@@ -0,0 +1,9 @@
+export const BurgerOpenIcon = () => {
+  return (
+    <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 17H28' stroke='currentColor' strokeWidth='3' strokeLinecap='round' />
+      <path d='M4 7L28 7' stroke='currentColor' strokeWidth='3' strokeLinecap='round' />
+      <path d='M4 27L28 27' stroke='currentColor' strokeWidth='3' strokeLinecap='round' />
+    </svg>
+  );
+};
