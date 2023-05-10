fac1232a603e3ee75d5c363adca1cdac82b01081
= <alexanderglazzkov@gmail.com>
2023-05-10 12:46:06 +0700

    created footer-component

--- src/components/blocks/footer/Footer.style.jsx
+++ src/components/blocks/footer/Footer.style.jsx
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled.footer`
+  background-color: ${(props) => props.theme.colors.background_footer};
+  color: ${(props) => props.theme.colors.text_footer};
+  font-size: 13px;
+  line-height: 18px;
+
+  @media ${(props) => props.theme.mediaQueries.tablet} {
+    font-size: 18px;
+    line-height: 25px;
+  }
+
+  @media ${(props) => props.theme.mediaQueries.desktop} {
+    font-size: 14px;
+    line-height: 19px;
+  }
+`;
