d67fd362952b6670a15d06577759ff248662233f
= <alexanderglazzkov@gmail.com>
2023-05-10 12:37:35 +0700

    created icon components

---
+++ src/components/icons/CrossIcon.jsx
@@ -0,0 +1,8 @@
+export const CrossIcon = () => {
+  return (
+    <svg width='8' height='8' viewBox='0 0 8 8' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M7 1L1 7' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M7 7L1 1' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  );
+};
