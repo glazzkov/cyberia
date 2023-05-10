d01bb1a18ca0f7d6e909dba4a8408687e02b8c50
= <alexanderglazzkov@gmail.com>
2023-05-10 12:46:38 +0700

    added breadcrumbs to 404 page

--- src/components/pages/page-404/Page404.jsx
+++ src/components/pages/page-404/Page404.jsx
@@ -1,5 +1,11 @@
 import { StyledPage404 } from './Page404.style';
+import { Breadcrumbs } from '../../blocks/breadcrumbs/Breadcrumbs';
 
 export const Page404 = () => {
-  return <StyledPage404>Страница не найдена</StyledPage404>;
+  return (
+    <StyledPage404>
+      <Breadcrumbs></Breadcrumbs>
+      <div>Страница не найдена</div>
+    </StyledPage404>
+  );
 };
